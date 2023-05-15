cacecebb0425100bff15b7b1fa1dc7177ca00756
KevinSalgado <sgkevinsg@gmail.com>
2023-05-15 14:10:04 -0600

    debug = true para que el navegador nos muestre los errores en la ejecucion

--- Cognitive_Crusaders_Ent/Cognitive_Crusaders_Ent/settings.py
+++ Cognitive_Crusaders_Ent/Cognitive_Crusaders_Ent/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+bi5nxj4u-z4n#x+865ml(6khq+-+g(i6c21*rzlx*1^47y$2k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
