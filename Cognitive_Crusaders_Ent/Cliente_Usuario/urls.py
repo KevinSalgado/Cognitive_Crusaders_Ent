84841ed0d0dfd0caedc8059e825b2cd2aee33a4b
KevinSalgado <sgkevinsg@gmail.com>
2023-05-15 20:54:33 -0600

    modificacion de hoja de estilos (bosquejo)

--- Cognitive_Crusaders_Ent/Cliente_Usuario/urls.py
+++ Cognitive_Crusaders_Ent/Cliente_Usuario/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.Inicio, name='Inicio'),
+    path('servicios/', views.Servicios, name='Servicios'),
 ]
--- Cognitive_Crusaders_Ent/Cliente_Usuario/views.py
+++ Cognitive_Crusaders_Ent/Cliente_Usuario/views.py
@@ -5,3 +5,7 @@ from django.template import loader
 def Inicio(request):
     template = loader.get_template("Inicio.html")
     return HttpResponse(template.render())
+
+def Servicios(request):
+    template = loader.get_template("servicios.html")
+    return HttpResponse(template.render())
