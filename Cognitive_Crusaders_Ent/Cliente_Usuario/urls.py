b9180b3d3e9970935e2f1c98757b62c40d635fa5
KevinSalgado <sgkevinsg@gmail.com>
2023-05-16 18:47:34 -0600

    Inicio de sion sencillo

--- Cognitive_Crusaders_Ent/Cliente_Usuario/urls.py
+++ Cognitive_Crusaders_Ent/Cliente_Usuario/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.Inicio, name='Inicio'),
     path('servicios/', views.Servicios, name='Servicios'),
+    path('salir/', views.Salir, name='Salir'),
 ]
--- Cognitive_Crusaders_Ent/Cliente_Usuario/views.py
+++ Cognitive_Crusaders_Ent/Cliente_Usuario/views.py
@@ -1,12 +1,18 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
-@login_required()
 def Inicio(Request):
     template = loader.get_template("Inicio.html")
     return HttpResponse(template.render())
 
+@login_required()
 def Servicios(Request):
     template = loader.get_template("servicios.html")
     return HttpResponse(template.render())
+
+def Salir(Request):
+    logout(Request)
+    template = loader.get_template("Inicio.html")
+    return HttpResponse(template.render()) 
