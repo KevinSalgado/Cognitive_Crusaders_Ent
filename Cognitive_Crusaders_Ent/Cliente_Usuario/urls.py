8650e64c2d9161a1dff1bd7de52f6a2a75c35a17
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2023-05-23 20:35:26 -0600

    agrega path para mandar a llamar a la nueva landing page

--- Cognitive_Crusaders_Ent/Cliente_Usuario/urls.py
+++ Cognitive_Crusaders_Ent/Cliente_Usuario/urls.py
@@ -2,9 +2,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.Inicio, name='Inicio'),
+    path('', views.index, name='index'), # views.index
     path('servicios/', views.Servicios, name='Servicios'),
     path('salir/', views.Salir, name='Salir'),
     path('register/', views.Register, name='Register'),
     path('AgregarTrabajadores/', views.AgregarTrabajadores, name='AgregarTrabajadores'),
+    # path('index/', views.index, name='index')
 ]
