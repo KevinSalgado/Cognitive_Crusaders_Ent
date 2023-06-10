8da9540075b2dc06c267b2b3089bc67ad5fabeec
KevinSalgado <sgkevinsg@gmail.com>
2023-06-10 17:55:26 -0600

    INSERTS.SQL que no sirven fueron removidos

--- INSERTS/INSERT_CLIENTE.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO public."Cliente_Usuario_cliente" (nombre, apellido, telefono, correo, contrasena, "fk_Rol_id", "Observaciones")
-VALUES 
-('Juan', 'Pérez', '555-1234', 'juan.perez@gmail.com', 'password123', 3, 'Este cliente es muy amable.'),
-('María', 'García', '555-5678', 'maria.garcia@yahoo.com', 'password456', 3, 'Este cliente siempre paga a tiempo.'),
-('Pedro', 'Martínez', '555-9012', 'pedro.martinez@hotmail.com', 'password789', 3, 'Este cliente tiene un pedido pendiente.');
